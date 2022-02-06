c14d3f1169dbf1451c53d602072a152fecbbad65
Andrés Moreno <andresfib@gmail.com>
2022-02-06 17:12:55 +0200

    Add timer for slideshow

--- slideshow_controller.py
+++ slideshow_controller.py
@@ -1,12 +1,17 @@
 import logging
-import os
+from datetime import datetime
 from pathlib import Path
+
+from croniter import croniter
+
+_LOGGER = logging.getLogger(__name__)
 
 class SlideshowController:
     """Controller to handle the slideshow"""
 
     def __init__(self, sonos_settings):
         self.show_slideshow = getattr(sonos_settings, "show_slideshow", False)
+        self.slideshow_timers = getattr(sonos_settings, "slideshow_timers", [])
         if self.show_slideshow:
             try:
                 images_path = getattr(sonos_settings, "images_path", None)
@@ -21,6 +26,7 @@ class SlideshowController:
                 _LOGGER.error("There are no png files in path: %s, slideshow not started", images_path)
             self.next_image_index = 0
 
+
     def get_next_image(self):
         if self.is_enabled():
             next_image = self.images_files[self.next_image_index]
@@ -30,5 +36,7 @@ class SlideshowController:
             _LOGGER.error("Not getting next image slideshow is disabled!")
 
     def is_enabled(self):
-        return self.show_slideshow
+        time = datetime.datetime.now()
+        is_enabled = self.show_slideshow and reduce(lambda x,y: x or croniter.match(y, time), self.slideshow_timers, True)
+        return is_enabled
 
